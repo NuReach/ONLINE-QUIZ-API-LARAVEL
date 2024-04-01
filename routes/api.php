9dc72b0e9cc47da3d7a985451c0920391d23b958
NuReach <nureach46@gmail.com>
2024-04-01 14:30:50 +0700

    teacher middleware

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum','admin'])->group(function () {
+Route::middleware(['auth:sanctum','admin','teacher'])->group(function () {
 
     Route::controller(CourseController::class)->group(function () {
         Route::get('/courses/{id}', 'getOneCourse');
